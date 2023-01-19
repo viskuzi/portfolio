588ef1eb4a3d36b609c12f27b7d2c0d8bb881810
Константин Лущик <viskuzi1987@gmail.com>
2023-01-19 19:53:18 +0100

    add menu

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,14 +2,16 @@ import React from "react";
 import style from './Header.module.scss'
 import Nav from "../nav/Nav";
 import { Zoom } from "react-awesome-reveal";
+import BurgerNav from "./burgerNav/BurgerNav";
 
 function Header() {
   return (
     <div className={style.header}>
         <div className={style.emailBut}>
-        <Zoom><span>kanstantsin.lushchyk@gmail.com</span></Zoom>
+          <Zoom><span>kanstantsin.lushchyk@gmail.com</span></Zoom>
 			  </div>
       <Nav />
+      <BurgerNav />
     </div>
   );
 }
---
+++ src/header/burgerNav/BurgerNav.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Roll } from "react-awesome-reveal";
+import style from './BurgerNav.module.scss';
+import { Link } from 'react-scroll';
+
+function BurgerNav() {
+  const [ menuIsOpen, setMenuIsOpen ] = useState(false);
+
+  const onBurgerMenu =() => {
+    setMenuIsOpen(!menuIsOpen);
+    console.log('menuisopen', menuIsOpen)
+  }
+
+  return (
+      <div className={style.burgerNav}>
+        <div className={menuIsOpen ? `${style.burgerNavItems} ${style.show}` : `${style.burgerNavItems}`}>
+        <Roll>
+          <Link className={style.navItem}  activeClass={style.active} to="home" spy={true} smooth={true} offset={-60} duration={1000}>
+            Home
+          </Link>
+          <Link className={style.navItem}  to="projects" spy={false} smooth={true} offset={0} duration={1000}>
+            Projects
+          </Link>
+          <Link className={style.navItem}  to="skills" spy={false} smooth={true} offset={0} duration={1000}>
+            Skills
+          </Link>
+          <Link className={style.navItem}  to="contacts" spy={false} smooth={true} offset={0} duration={1000}>
+            Contacts
+          </Link>
+        </Roll>
+        </div>
+        <div onClick={onBurgerMenu} className={style.menuButton}></div>
+      </div>
+  );
+}
+
+export default BurgerNav;
